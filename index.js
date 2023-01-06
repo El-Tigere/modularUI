2ae76528570b513b5a1ce53ccbd934af07f15662
El_Tigere <eltigere@web.de>
2023-01-06 15:12:23 +0100

    resources can now be requested from the server

--- index.js
+++ index.js
@@ -4,12 +4,42 @@ const fs = require('fs');
 const port = 8080;
 const host = '127.0.0.1';
 
-const app = require('./app');
+const mimeTypes = JSON.parse(fs.readFileSync('mime.json'));
+
+const app = require('./page/app');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'ContentType': 'text/html'});
-    let page = app.main.render().trim();
-    res.end(page);
+    
+    // respond with 404 to all requests with special chars in the url (except / and . (but not ..))
+    const url = (((req.url || '/').match(/^([\w\d/]\.?)+$/g) || [''])[0].toLowerCase()).trim();
+    if(url == '') {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+    
+    // respond with the main page when the url '/' is requested
+    if(url == '/') {
+        res.writeHead(200, {'ContentType': 'text/html'});
+        let page = app.main.render().trim();
+        res.end(page);
+        return;
+    }
+    
+    // respond with resources
+    // TODO: add a way to use client side external js files
+    if(!url.endsWith('.js') && fs.existsSync('page/' + url)) {
+        const ending = (url.match(/\.[\w\d]+$/) || [])[0];
+        if(ending && mimeTypes[ending]) {
+            res.writeHead(200, {'ContentType': mimeTypes[ending]});
+            res.end(fs.readFileSync('page/' + url));
+        } else {
+            res.writeHead(415);
+            res.end();
+        }
+        return;
+    }
+    
 });
 
 server.listen(port, host, () => {
