ff8b604e15005b288c5c6c0d1df1773462c64d9b
El_Tigere <eltigere@web.de>
2023-01-06 15:16:27 +0100

    added an example page

---
+++ page/app.js
@@ -0,0 +1,40 @@
+const {Element} = require('../renderer');
+
+const namespaces = {
+    'app': require('./app'),
+    'logos': require('./logos/logos')
+};
+
+exports.main = new Element(namespaces, (content, args) => `
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Testseite</title>
+        <link rel="stylesheet" href="style/common.css">
+    </head>
+    <body>
+        <app:mainbody></app:mainbody>
+    </body>
+</html>
+`);
+
+exports.mainbody = new Element(namespaces, (content, args) => `
+<header>
+    <logos:logo size=128></logos:logo>
+    <span>abc</span>
+</header>
+<main>
+    <logos:logo size=400></logos:logo>
+    <logos:logo size=200></logos:logo>
+    <logos:logo size=100></logos:logo>
+    <logos:logo size=50></logos:logo>
+    <logos:logo size=25></logos:logo>
+    <p>def</p>
+</main>
+<footer>
+    <p>xyz</p>
+</footer>
+`);
