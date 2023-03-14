46a5c8008274887c2feacaa990794d4d629e5a19
El_Tigere <eltigere@web.de>
2023-03-14 18:13:33 +0100

    made logo element more readable

--- page/util/logos.m.js
+++ page/util/logos.m.js
@@ -1,5 +1,6 @@
 const {Element} = require('../../renderer');
 
-exports.logo = new Element({}, (content, args) => `
-<img src="/resources/text_logo_${args.size ? (args.size <= 64 ? 64 : args.size <= 128 ? 128 : 256) : 128}.png" width=${args.size} alt="logo">
-`);
+exports.logo = new Element({}, (content, args) => {
+    const s = args.size ? (args.size <= 64 ? 64 : args.size <= 128 ? 128 : 256) : 128;
+    return `<img src="/resources/text_logo_${s}.png" width=${args.size || 128} alt="logo">`;
+});
