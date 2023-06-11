7d79ddb6089db84ea6bf2eab815f9bc5d24dc418
El_Tigere <eltigere@web.de>
2023-06-11 19:38:40 +0200

    added login function

--- database.js
+++ database.js
@@ -21,6 +21,27 @@ function connect(connectionConfig) {
 }
 exports.connect = connect;
 
+/**
+ * Searches a user with matching login data and returns the id (or -1 when none was found).
+ * @param {string} username 
+ * @param {string} password 
+ * @param {(id : number) => {}} callback 
+ */
+function login(username, password, callback) {
+    const escapedUserName = mysql.escape(username);
+    const escapedPWHash = mysql.escape(sha256(password));
+    const query = `SELECT ID FROM users WHERE Name = ${escapedUserName} AND PWHash = ${escapedPWHash}`;
+    connection.query(query, (err, result) => {
+        if(err) throw err;
+        if(result.length > 0) {
+            callback(result[0]['ID']);
+        } else {
+            callback(-1);
+        }
+    });
+}
+exports.login = login;
+
 function sha256(input) {
     let hash = crypto.createHash('sha256').update(input).digest('hex');
     return hash;
