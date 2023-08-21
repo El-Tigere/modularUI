b0f41bd24ec68a78fa4997726634f430b45d4695
El_Tigere <eltigere@web.de>
2023-08-21 20:26:51 +0200

    corrected register form names

--- page/profile/register/register.m.js
+++ page/profile/register/register.m.js
@@ -19,9 +19,9 @@ exports.elements.form = new Element({preRender: true}, (content, args) => `
     <fieldset>
         <legend>register</legend>
         <table>
-            <tr><td>username:</td><td><input id="username" type="text" name="rUsername"></td></tr>
-            <tr><td>password:</td><td><input id="password" type="password" name="rPassword"></td></tr>
-            <tr><td>repeat password:</td><td><input id="password2" type="password" name="rPassword2"></td></tr>
+            <tr><td>username:</td><td><input id="username" type="text" name="username"></td></tr>
+            <tr><td>password:</td><td><input id="password" type="password" name="password"></td></tr>
+            <tr><td>repeat password:</td><td><input id="password2" type="password" name="password2"></td></tr>
         </table>
         <input type="submit" value="create account">
     </fieldset>
